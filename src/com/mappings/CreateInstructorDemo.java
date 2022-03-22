b5a04b3827c930d535a31124a052441c7c60e1a0
Harshazemoso <harshavardhan.danda@zemosolabs.com>
2022-03-22 09:02:44 +0530

    Initial commit

---
+++ src/com/mappings/CreateInstructorDemo.java
@@ -0,0 +1,44 @@
+package com.mappings;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructorDemo {
+    public static void main(String[] args){
+
+        //create session factory
+        SessionFactory factory= new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        //create session
+        Session session= factory.getCurrentSession();
+        try{
+
+            Instructor tempInstructor= new Instructor("harsha", "vardhan", "harshavardhan@gmail.com");
+
+            InstructorDetail tempInstructorDetail= new InstructorDetail("http://www.harsha.com", "love to code");
+
+            //link objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+            //start a transaction
+            session.beginTransaction();
+
+            //save the instructor
+            System.out.println("saving instructor: " +tempInstructor);
+            session.save(tempInstructor);//this will also save details of detail , cuz of cascade all
+
+            //commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("completed!!");
+        }finally{
+            session.close();
+            factory.close();
+        }
+    }
+}
